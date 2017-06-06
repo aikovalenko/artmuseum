a7dcb6a7c0d43f8775a2b988595f20311c239d45
aikovalenko <kovalenko.ai@yandex.ru>
2017-06-06 17:13:34 +0200

    Init files

---
+++ src/js/libs.js
@@ -0,0 +1,5 @@
+//= ../libs/jquery/dist/jquery.min.js
+//= ../libs/modernizer/modernizr.js
+//= ../libs/fastclick/lib/fastclick.js
+//= ../libs/platform.js/platform.js
+
---
+++ src/js/scripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("html").addClass(platform.name.toLowerCase());
+    $("html").addClass(platform.os.family.toLowerCase());
+});
