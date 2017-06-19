15100c3b3d17ed50545da5b21519b7463233ee0d
aikovalenko <kovalenko.ai@yandex.ru>
2017-06-19 19:42:37 +0200

    header/new fnc

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-    $("html").addClass(platform.name.toLowerCase());
-    $("html").addClass(platform.os.family.toLowerCase());
+    $('html').addClass(platform.name.toLowerCase()).addClass(platform.os.family.toLowerCase());
+
+    $('.js-call-search').on('click', function (e) {
+        e.stopPropagation();
+        $('.search-block').addClass('opened');
+    });
+    $(document).on('click', function (e) {
+        // Do whatever you want; the event that'd fire if the "special" element has been clicked on has been cancelled.
+    });
 });
