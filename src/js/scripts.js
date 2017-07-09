febaedcf257da7d208c2759f9d63bed2d9a59ae6
aikovalenko <kovalenko.ai@yandex.ru>
2017-07-09 16:32:34 +0300

    комментарии к js/фикс для мобильного меню/нет дерганий слайдерам при загрузке

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
+//дебаунс для правильного ресайза
 function debounce(func, wait, immediate) {
     var timeout;
     return function() {
@@ -13,6 +14,7 @@ function debounce(func, wait, immediate) {
     };
 }
 
+//туггл для функций
 jQuery.fn.clickToggle = function(a,b) {
     function cb(){ [b,a][this._tog^=1].call(this); }
     return this.on("click", cb);
@@ -20,101 +22,91 @@ jQuery.fn.clickToggle = function(a,b) {
 
 
 
+
+
 $(document).ready(function() {
 
+    //определим девайс и ОС и добавим класс к <html/>
     $('html').addClass(platform.name.toLowerCase()).addClass(platform.os.family.toLowerCase());
 
 
-    var CallControl = $('.js-call-control'),
+    //десктоп панель настроек
+    var callControl = $('.js-call-control'),
         mainControlsSectionBlock = $('.main-controls-section-block');
 
-    CallControl.click(function(e) {
+    callControl.click(function(e) {
 
         e.stopPropagation();
 
-        $(this).next(mainControlsSectionBlock).toggleClass('opened');
-        $(this).toggleClass('active');
+        $(this).find(mainControlsSectionBlock).toggleClass('opened');
+        $(this).find(".main-controls__button").toggleClass('active');
 
-        $('.js-call-control').not(this).next(mainControlsSectionBlock).each(function () {
-            $(this).removeClass('opened');
-            $(this).prev(CallControl).removeClass('active');
+        callControl.not(this).each(function () {
+            $(this).find(mainControlsSectionBlock).removeClass('opened');
+            $(this).find(".main-controls__button").removeClass('active');
         });
 
-    });
 
+    });
     $('.js-call-search').click(function(e) {
         $('.form-search input').focus();
     });
-
+    mainControlsSectionBlock.click(function(e){
+        e.stopPropagation();
+    });
     $(document).click(function() {
-        CallControl.next(mainControlsSectionBlock).removeClass('opened');
-        CallControl.removeClass('active');
+        callControl.find(mainControlsSectionBlock).removeClass('opened');
+        callControl.find(".main-controls__button").removeClass('active');
     });
 
 
+    //меню-бургер
     var burger = $('.js-burger'),
         menu = $('.menu-mobile'),
         close = $('.js-notifs-close'),
         notifsTop = $('.header__notifications__inner');
 
     burger.click(function () {
-
         notifsTop.toggleClass('hidden');
         burger.toggleClass("active");
         menu.toggleClass("opened");
-
-
-        // $('html').toggleClass('overflowHidden');
-
-
-
-
-
     });
-
     close.click(function() {
         notifsTop.addClass('closed');
     });
 
-    $('.js-mobile-menu').click(function() {
 
-
-
-        var block = $('.menu-mobile__panels');
-
+    //мобильная версия панели-настроек
+    var mobileMenu = $('.js-mobile-menu');
+    mobileMenu.click(function() {
+        var panels = $('.menu-mobile__panels'),
+            list = $('.menu-mobile__list'),
+            panelsBlock = $('.menu-mobile__panels__block'),
+            copyright = $('.menu-mobile__copyright');
 
         $(this).toggleClass("active");
-        $('.menu-mobile__panels__block').removeClass("active");
+        panelsBlock.removeClass("active");
 
         var name = $(this).data("name");
-        console.log(name);
+
         $('.' + name).addClass('active');
-
-
-        $('.js-mobile-menu').not(this).each(function () {
+        mobileMenu.not(this).each(function () {
             $(this).removeClass('active');
-
         });
-
-
-        if ($('.js-mobile-menu').hasClass('active')) {
-
-
-            $('.menu-mobile__list').addClass('hide');
-            block.removeClass('hide');
-            $('.menu-mobile__copyright').addClass("absolute");
+        if (mobileMenu.hasClass('active')) {
+            list.addClass('hide');
+            panels.removeClass('hide');
+            copyright.addClass("absolute");
 
         } else {
-            $('.menu-mobile__list').removeClass('hide');
-            block.addClass('hide');
-            $('.menu-mobile__copyright').removeClass("absolute");
+            list.removeClass('hide');
+            panels.addClass('hide');
+            copyright.removeClass("absolute");
         }
-
-
     });
 
 
-
+    //пункт Внимание в мобильной версии /можно заменить на аккордион/
     $('.js-call-notifs').click(function(e) {
         e.preventDefault();
         var notifs = $('.mobile-notifs'),
@@ -130,26 +122,26 @@ $(document).ready(function() {
         } else {
             notifs.css('height', 0);
         }
-
     });
 
 
-
+    //закрывашка для мобильного поиска
     $('.form--mobile-search').on("input", function() {
         if( $(this).val().length > 0 ) {
             $('.js-clear-search').addClass('show');
         } else $('.js-clear-search').removeClass('show');
 
     });
-
     $('.js-clear-search').click(function() {
         $('.form--mobile-search').val('');
         $(this).removeClass('show');
-
     });
 
 
-
+    //слайдер на главной странице
+    /*
+    у ссылки в слайдере на событие есть data-ticket, который тянется к кнопке Купить под слайдером
+     */
     var mainSlider = $('.js-main-slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -170,57 +162,33 @@ $(document).ready(function() {
         ]
     }).on('afterChange', function(event, slick, currentSlide, nextSlide){
         var link = $(slick.$slides.get(currentSlide)).data('ticket');
+
         $('.slider-btn-buy').attr('href', link);
     });
     var link = $('.slick-slide.slick-current.slick-active').data('ticket');
+
     $('.slider-btn-buy').attr('href', link);
 
 
-
-
-
+    //кнопки переключеня контентом в секции, и в афише кнопка фильтра
     $('.js-content-control').each(function () {
-
-        var block =  $(this);
+        var block = $(this);
 
         $(this).find('.js-content-control-btn').click(function (e) {
-
             e.preventDefault();
             $(this).addClass('active');
-
-
             block.find('.js-content-control-btn').not(this).each(function () {
                 $(this).removeClass('active');
                 $('.section--afisha__filter').removeClass('opened');
             });
         });
     });
-
     $('.js-call-filter').click(function() {
         $('.section--afisha__filter').addClass('opened');
     });
 
 
-
-    // $('.accordion-title').click(function(e) {
-    //     e.preventDefault();
-    //     if ($(window).width() < 1024) {
-    //
-    //         var $this = $(this),
-    //             time = 500;
-    //
-    //         if ($this.next().hasClass('show')) {
-    //             $this.next().removeClass('show');
-    //             $this.next().slideUp(time);
-    //         } else {
-    //             $this.parent().parent().find('.accordion-content').removeClass('show');
-    //             $this.parent().parent().find('.accordion-content').slideUp(time);
-    //             $this.next().toggleClass('show');
-    //             $this.next().slideToggle(time);
-    //         }
-    //     }
-    // });
-    //
+    //трансформация списка в аккордион в мобиле
     function accordion() {
         var content = $('.accordion--mobile .accordion-content');
 
@@ -233,18 +201,16 @@ $(document).ready(function() {
         else {
             content.addClass('maxHeight');
         }
-
         $('.js-accordion').accordion({
             "transitionSpeed": 400
         });
-
     }
+    accordion();
 
 
+    //переключения между двумя списками кнопок управления контентом
     function headerControl() {
-
         $('.section__header__controls').each(function () {
-
             var first = $(this).find('.first'),
                 second = $(this).find('.second'),
                 firstWidth = first.width(),
@@ -268,10 +234,10 @@ $(document).ready(function() {
             });
         });
     }
-
     headerControl();
 
 
+    //слайдер афиша с адаптивом
     var afishaSliderSets = {
         infinite: true,
         slidesToShow: 1,
@@ -280,18 +246,15 @@ $(document).ready(function() {
         variableWidth: true,
         dots: false,
         arrows: true
-    };
-
-    var afishaSlider = $('.js-multiple-items-slider').slick(afishaSliderSets);
+    },
+        afishaSlider = $('.js-multiple-items-slider').slick(afishaSliderSets);
 
     function afishaSliderSettings() {
-
         function unwrap() {
             afishaSlider.slick('unslick');
             $('.js-multiple-items-slider .slide .item').unwrap();
             $('.js-multiple-items-slider .slide').remove();
         }
-
         function reset(num) {
             unwrap();
             var divs = $(".item");
@@ -299,7 +262,6 @@ $(document).ready(function() {
                 divs.slice(i, i + num).wrapAll("<div class='slide'></div>");
             }
         }
-
         if ($(window).width() < 1280) {
             reset(3);
             afishaSlider.slick(afishaSliderSets);
@@ -313,16 +275,16 @@ $(document).ready(function() {
             unwrap();
         }
     }
+    afishaSliderSettings();
 
+
+    //для видео
     $('video').mediaelementplayer({
         features: ['']
     });
 
-    afishaSliderSettings();
-    accordion();
 
-
-
+    //правильный пересчет функций на ресайз
     var resizeFn = debounce(function() {
         accordion();
         afishaSliderSettings();
